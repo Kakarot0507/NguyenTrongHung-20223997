882458b6f52fae407a89f940b5eb3841797a52b6
Nguyen Trong Hung <Hung.NT223997@sis.hust.edu.vn>
2024-12-24 00:01:38 +0700

    Add files via upload

---
+++ tronghung.cpp
@@ -0,0 +1,50 @@
+﻿C++
+
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left, * right;
+    int height; 
+};
+Node* newNode(int data) {
+    Node* node = new Node();
+    node->data = data;
+    node->left = node->right = NULL;
+    node->height = 1;
+    return node;
+}
+//int height(Node* N) {// chieu cao cay
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+int getBalance(Node* N) {
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    // Xoay
+    x->right = y;
+    y->left = T2;
+
+    // Cập nhật chiều cao
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    // Trả về nút gốc mới
+    return x;
+}
+void inOrder(Node* root) {
+    if (root != NULL) {
+        inOrder(root->left);
+        cout << root->data << " ";
+        inOrder(root->right);
+    }
+}
